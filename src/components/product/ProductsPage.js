a487e988399fae10a0a51575f1cbc4ee90d84072
unknown <clang8787.com>
2018-03-29 18:11:26 -0400

    add click message to product page

--- src/components/product/ProductsPage.js
+++ src/components/product/ProductsPage.js
@@ -1,10 +1,42 @@
-import React from 'react'; 
+import React, {PropTypes} from 'react'; 
 
 class ProductsPage extends React.Component {
+    constructor(props, context){
+        super(props, context);
+
+        this.state = {
+            product: {title: ""}
+        };
+
+        this.onTitleChange= this.onTitleChange.bind(this);
+        this.onClickSave= this.onClickSave.bind(this);
+    }
+
+    onTitleChange(event){
+        const product = this.state.product;
+        product.title = event.target.value;
+        this.setState({product: product});
+    }
+
+    
+    onClickSave(){
+        alert(`Saving ${this.state.product.title}`);  
+        //alert("saving");  
+    }
+
     render() {
         return (
-            <div className="jumbotron">
-                <h1>ProductsPage</h1> 
+            <div>
+                <h1>Products</h1> 
+                <h2>Add Product</h2>
+                <input
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.product.title} />
+                <input
+                    type="submit"
+                    value="Save"
+                    onClick={this.onClickSave} />
             </div>
         );
     }
